b6cce872ec6049b387e046994d902115164f0e5d
cassymarie <cassy2205@gmail.com>
2021-03-09 17:20:52 -0700

    commit tree 9f424a4d77bc603437d5d0c0de08f9aff708340c

--- js/createStore.js
+++ js/createStore.js
@@ -1,4 +1,28 @@
-let state;
+
+// //it's not very good if this variable is global, and we can accidentally overwrite
+// let state;
+
+// // We can solve this by wrapping our state in a function
+// function createStore(){
+//   let state;
+// }
+
+// to keep the code generic to any application, pass in the reducer so the store has access
+function createStore(reducer) {
+  let state;
+  // state is now accessible to dispatch
+
+  function dispatch(action) {
+    state = reducer(state, action);
+    render();
+  }
+
+  function getState() {
+    return state;
+  }
+
+  return { dispatch, getState }
+} 
 
 function reducer(state = { count: 0 }, action) {
   switch (action.type) {
@@ -10,19 +34,26 @@ function reducer(state = { count: 0 }, action) {
   }
 };
 
-function dispatch(action){
-  state = reducer(state, action);
-  render();
-};
+// // MOVED into createStore to have access to state
+// // ~ however ~ render then loses state and throws an error
+// function dispatch(action){
+//   state = reducer(state, action);
+//   render();
+// };
 
 function render() {
   let container = document.getElementById('container');
-  container.textContent = state.count;
+  // container.textContent = state.count; // no longer have access to state
+  container.textContent = store.getState().count
 };
 
-dispatch({ type: '@@INIT' })
+// dispatch({ type: '@@INIT' }) // now located within store
+
+let store = createStore(reducer); //pass in the reducer to have access to know what to do with state
+store.dispatch({ type: '@@INIT' });
 let button = document.getElementById('button');
 
-button.addEventListener('click', function() {
-    dispatch({ type: 'INCREASE_COUNT' });
-})
+button.addEventListener('click', () => {
+  // dispatch({ type: 'INCREASE_COUNT' }); // need to access store
+    store.dispatch({ type: 'INCREASE_COUNT' });
+}) 
